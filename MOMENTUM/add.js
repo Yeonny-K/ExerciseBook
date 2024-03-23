82f6063b64099290eef2f69148ef8555290a4846
Yeonny <164313457+Yeonny-K@users.noreply.github.com>
2024-03-23 17:27:38 +0900

    Add files via upload

--- MOMENTUM/add.js
+++ MOMENTUM/add.js
@@ -1,4 +1,25 @@
-const amIFat = null;
-let something;
+const calculator = {
+    add: function(a, b){
+        return a + b;
+    },
+    minus: function(a, b){
+        return a - b;
+  },
+    div: function(a, b){
+        return a / b ;
+ },
+    pow: function(a, b){
+        return a**b;
+    },
+};
 
-console.log(something, amIFat);
+const plusResult = calculator.add(2, 3);
+const minusResult = calculator.minus(10, plusResult);
+const divResult = calculator.div(100, minusResult);
+const powResult = calculator.pow(plusResult, minusResult);
+
+
+console.log(plusResult);
+console.log(minusResult);
+console.log(divResult);
+console.log(powResult);
