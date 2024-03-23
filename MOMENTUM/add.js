e07d1e13a52316a9420591be67062a4f2dea74be
Yeonny <164313457+Yeonny-K@users.noreply.github.com>
2024-03-23 20:24:17 +0900

    Add files via upload

--- MOMENTUM/add.js
+++ MOMENTUM/add.js
@@ -1,25 +1,16 @@
-const calculator = {
-    add: function(a, b){
-        return a + b;
-    },
-    minus: function(a, b){
-        return a - b;
-  },
-    div: function(a, b){
-        return a / b ;
- },
-    pow: function(a, b){
-        return a**b;
-    },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.add(2, 3);
-const minusResult = calculator.minus(10, plusResult);
-const divResult = calculator.div(100, minusResult);
-const powResult = calculator.pow(plusResult, minusResult);
+if(isNaN(age) || age < 0){
+    console.log("Please enter a real positive number");
+} else if(age < 18){
+    console.log("You are too young");
+} else if(age >= 18 && age <= 50){
+    console.log("You can drink");
+} else if(age > 50 && age <= 80)  {
+    console.log("I think you should quit drinking.");
+} else if(age === 100){
+    console.log("wow you are wise");
+} else if(age > 80) {
+    console.log("You can do whatever you want.");
+}
 
-
-console.log(plusResult);
-console.log(minusResult);
-console.log(divResult);
-console.log(powResult);
