d62f4ea088462098d6fc0aeb048e0145fb9569a4
Yeonny <164313457+Yeonny-K@users.noreply.github.com>
2024-03-25 00:37:23 +0900

    Add files via upload

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,6 +1,33 @@
-const title = document.getElementById("title");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-title.innerText = "Got you!";
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 
-console.log(title.id);
-console.log(title.className);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const inputName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, inputName);
+    paintGreetings(inputName);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+if(savedUsername === null){
+    // form 보여주기
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+} else{
+    // h1 보여주기
+    paintGreetings(savedUsername);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
